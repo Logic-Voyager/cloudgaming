2ce8291314e7db2589e22e160cb61e8e3846a3bc
Logic-Voyager <shrutisharma.m31@gmail.com>
2026-04-12 01:18:48 +0530

    Deploying on render

--- models/User_Profile.js
+++ models/User_Profile.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema({
     gamertag: { type: String, unique: true },
     email: { type: String, unique: true },
     hashed_password: { type: String, required: true },
-    country: String,
+    country:{type: String, required: true},
     account_status: { type: String, enum: ['ACTIVE', 'BANNED', 'SUSPENDED'], default: 'ACTIVE' }
 });
 module.exports = mongoose.model('User_Profile', UserSchema);
