62a4f6e6596b95fc5a514f78785dae024d24772e
Logic-Voyager <shrutisharma.m31@gmail.com>
2026-04-14 14:46:02 +0530

    Billing Schema Changed

--- models/Billing_History.js
+++ models/Billing_History.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const BillingSchema = new mongoose.Schema({
-    transaction_id: Number,
-    user_id: String,
+    transaction_id: {type: Number, unique:true},
+    email: String,
     amount: Number,
     payment_method: String,
-    payment_status: { type: String, enum: ['SUCCESS', 'PENDING', 'FAILED'] }
+    payment_status: { type: String, enum: ['SUCCESS', 'PENDING', 'FAILED'] },
+    createdAt: {type: Date, default: Date.now}
 });
 module.exports = mongoose.model('Billing_History', BillingSchema);
