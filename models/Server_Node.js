a558687a516fa2719cfc9b4569f6dab1ede8b6d0
Logic-Voyager <shrutisharma.m31@gmail.com>
2026-04-14 20:30:00 +0530

    seed

--- models/Server_Node.js
+++ models/Server_Node.js
@@ -3,7 +3,7 @@ const NodeSchema = new mongoose.Schema({
     node_id: Number,
     data_center: String,
     hardware_spec: String,
-    node_status: { type: String, enum: ['ONLINE', 'MAINTENANCE', 'FULL'] }
+    node_status: { type: String, enum: ['ONLINE', 'MAINTENANCE', 'OFFLINE'] }
 });
 module.exports = mongoose.model('Server_Node', NodeSchema);
 // module.exports = mongoose.model('Server_Node', NodeSchema, 'Server_Node');
